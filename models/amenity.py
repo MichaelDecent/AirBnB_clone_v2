676339399459ca20865ab6cbd4c731f4d90f4d53
MichaelDecent <michaeldecent2@gmail.com>
2023-08-24 12:27:37 +0000

    fixed error in console.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import place_amenity
 from sqlalchemy import Column, String
 from models import storage_type
 from sqlalchemy.orm import relationship
