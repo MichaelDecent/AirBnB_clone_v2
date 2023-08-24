98c41d8b7bc7ffe8fd0da36e5fe6a643a161decf
MichaelDecent <michaeldecent2@gmail.com>
2023-08-24 02:32:24 +0000

    updated amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -10,6 +10,6 @@ class Amenity(BaseModel, Base):
     
     if storage_type == "db":
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("place_amenity", back_populates="amenities")
+        place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
     else:
         name = ""
