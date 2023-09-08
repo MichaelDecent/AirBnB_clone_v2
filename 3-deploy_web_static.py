33399fb57ed783e550ee1e9d1715343cf51675e0
MichaelDecent <michaeldecent2@gmail.com>
2023-09-08 04:02:22 +0000

    added a file that fully deploys the web_statics to web servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+ a Fabric script (based on the file 2-do_deploy_web_static.py)
+ that creates and distributes an archive to your web servers,
+ using the function deploy
+"""
+
+from fabric.api import *
+from os.path import exists
+from datetime import datetime
+
+env.hosts = ['54.208.65.105', '18.209.152.183']
+env.user = 'ubuntu'
+env.key_filename = '/home/vagrant/.ssh/id_rsa'
+
+
+@runs_once
+def do_pack():
+    """ Generates a .tgz archive from the folder web_static folder
+    """
+    local("mkdir -p versions")
+
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = f"versions/web_static_{date}.tgz"
+    print(f"\nCreating a new_version of web_static: {archive_name}")
+    result = local(f"tar -cvzf {archive_name} web_static")
+    if result.succeeded:
+        return archive_name
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploys the web static to the server"""
+
+    if not exists(archive_path):
+        return False
+    try:
+        archive_name = archive_path.split('/')[-1]
+        file_name = archive_name.split('.')[0]
+        sym_link = "/data/web_static/current"
+        release_version = f"/data/web_static/releases/{file_name}/"
+
+        print(f"Deploying new_version from {archive_path}\n")
+        put(archive_path, f"/tmp/{archive_name}")
+        run(f"mkdir -p {release_version}")
+        run(f"tar -xzf /tmp/{archive_name} \
+-C {release_version} --strip-components=1")
+        run(f"rm /tmp/{archive_name}")
+        run(f"rm -f {sym_link}")
+        run(f"ln -s {release_version} {sym_link}")
+        print(f"\nNew Version Deployed --> {release_version}")
+        return True
+    except Exception as e:
+        print(f"\nFailed to Deploy New Version -->{release_version}\n{str(e)}")
+        return False
+
+
+def deploy():
+    """Fully deploys web_statics to web servers"""
+    archive_path = do_pack()
+
+    return do_deploy(archive_path) if archive_path else False
