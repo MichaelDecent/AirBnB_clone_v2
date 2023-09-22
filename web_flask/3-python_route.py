817df6af6c758413e9ecc0d48e16a781d725bc31
MichaelDecent <michaeldecent2@gmail.com>
2023-09-22 14:58:06 +0000

    a script that starts a Flask web application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+this module handle a route to my home page
+"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home_page():
+    """ This returns a string """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB_page():
+    """ Returns 'HBNB' """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """displays C with additional text"""
+    text = text.replace('_', ' ')
+    return f"C {escape(text)}"
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyhton_text(text=None):
+    """displays python with additional text"""
+    if text is None:
+        text = 'is cool'
+
+    text = text.replace('_', ' ')
+    return f"Python {escape(text)}"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=1)
