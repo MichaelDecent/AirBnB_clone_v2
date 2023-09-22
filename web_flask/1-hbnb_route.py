289a9d6cd101959c65a77e6fc0bc96d44b239443
MichaelDecent <michaeldecent2@gmail.com>
2023-09-22 13:43:19 +0000

    a script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,14 +4,16 @@ this module handle a route to my home page
 """
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 @app.route('/hbnb', strict_slashes=False)
-
-
 def hello_page():
     """ This returns a string """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
