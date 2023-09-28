d5119efc17b03a7e6a2ec532326e7c0aab7f17ce
MichaelDecent <michaeldecent2@gmail.com>
2023-09-28 11:10:14 +0000

    added more route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,6 +8,7 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     """This displays return an html of a list of states """
